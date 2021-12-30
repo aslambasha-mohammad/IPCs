b5ffc76d02b8268fdfcd29d2a02cbace297754e1
aslambasham <aslambasham@teamf1networks.com>
2021-12-30 17:08:30 +0530

    Adding sample shared memory programs

---
+++ shared_mem_recv.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/shm.h>
+
+#define	SHM_KEY		24
+#define MEM_SIZE	1024
+
+int main()
+{
+	int shmId = 0;
+	char *memPtr = NULL;
+
+	shmId = shmget(SHM_KEY, 0 /*MEM_SIZE*/, 0);	//No need of permission and mem size since shm is already created by sender
+	if(shmId < 0)
+	{
+		printf("Failed to get shared memory id for key '%d'\n", SHM_KEY);
+		return -1;
+	}
+
+	memPtr = shmat(shmId, NULL, 0);
+	if(memPtr != (void *)-1)	//on failure shmat returns (void *)-1
+	{
+		printf("Read '%s' from the shared memory\n", memPtr);
+	}
+	else
+		printf("Failed to read the data from shared memory\n");
+	return 0;
+}
---
+++ shared_mem_send.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/shm.h>	//shared memory sys calls
+
+#define	SHM_KEY		24
+#define	MEM_SIZE	1024
+
+int main()
+{
+	int shmId = 0;
+	char *memPtr = NULL;
+
+	shmId = shmget(SHM_KEY, MEM_SIZE, 0666|IPC_CREAT);
+	if(shmId < 0)
+	{
+		printf("Failed to get the Shared Memory Id with key '%d'\n", SHM_KEY);
+		return -1;
+	}
+
+	memPtr = shmat(shmId, NULL, 0);
+	if(memPtr != (void *)-1)	//on failure shmat returns (void *)-1
+	{
+		printf("Enter the data which need to be send via shared memory: ");
+		scanf("%[^\n]s", memPtr);
+		printf("'%s' written successfully into shared memory\n", memPtr);
+	}
+	else
+		printf("Failed to get the shared memory pointer\n");
+
+	return 0;
+}
